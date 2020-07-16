5b1ec888b8c3ad863cf3a22bed19cf1e00155e65
MrGMunoz <67342240+MrGMunoz@users.noreply.github.com>
2020-07-16 16:12:22 -0400

    Update cachematrix.R
    Updated functions makeCacheMatrix and cachemean. makeCacheMatrix creates a 2dim 4x4 matrix and sets functions modify it. cacheSolve, uses makeCacheMatrix to solve a an inversed matrix from it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Following example, a I made the function taking 'x' as a 2-dim 4x4 matrix
+## Changed 'mean' names to 'Inversed' to keep how function works as clear as posible
+makeCacheMatrix <- function(x = matrix(sample(100:1000 , 9) , nrow = 4 , ncol = 4)) {      
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInversed <- function(solve) m <<- solve
+        getInversed <- function() m
+        list(
+                set = set, 
+                get = get,
+                setInversed = setInversed,
+                getInversed = getInversed)
 }
 
-
-## Write a short comment describing this function
-
+## I continued following the example. I changed 'data' for 'receivedMatrix'.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## next line calls getInversed function nested inside makeCacheMatrix function
+        m <- x$getInversed()
+        if(!is.null(m)) {
+                message("full matrix received")
+                return(m)
+        }
+        receivedMatrix <- x$get()
+        m <- solve(receivedMatrix, ...)
+        x$setInversed(m)
+        return m
 }
